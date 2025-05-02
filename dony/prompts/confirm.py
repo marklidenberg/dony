59cf12812bc7dc609c8b3e45be5de7fa407a3fff
marklidenberg <marklidenber@gmail.com>
2025-05-02 09:46:22 +0200

    add dony

--- dony/prompts/confirm.py
+++ dony/prompts/confirm.py
@@ -5,7 +5,18 @@ from prompt_toolkit.styles import Style
 def confirm(
     message: str,
     default: bool = True,
+    provided_answer: str = None,
 ):
+    if provided_answer is not None:
+        if provided_answer.lower() in ["y", "yes", "true", "1"]:
+            return True
+        elif provided_answer.lower() in ["n", "no", "false", "0"]:
+            return False
+        else:
+            raise ValueError(
+                f"Provided answer '{provided_answer}' is not a valid boolean value. Use one of 'y', 'yes', 'true', '1', 'n', 'no', 'false', '0'."
+            )
+
     result = questionary.confirm(
         message=message,
         default=default,
