a4513bf7572e8429479a51402ee004551a74b8df
marklidenberg <88275928+marklidenberg@users.noreply.github.com>
2026-01-09 16:19:11 +0100

    chore: update donyfiles

--- donyfiles/release.py
+++ donyfiles/release.py
@@ -1,5 +1,6 @@
+from functools import partial
 from marklidenberg_donyfiles import release
-import dony
+import asyncio
 
 if __name__ == "__main__":
-    dony.command(run_from="git_root")(release)()
+    asyncio.run(release(path=__file__))
